db1f79d91caea4f1f8e915e05b1eda6a313ce875
Mike Brajkovich <mbrajk@users.noreply.github.com>
2021-12-29 23:19:08 -0600

    base implementation of observer pattern

---
+++ ObserverPattern/2-ObserverImplementation/Observers/IObserver.cs
@@ -0,0 +1,6 @@
+﻿namespace ObserverPattern._2_ObserverImplementation.Observers;
+
+public interface IObserver
+{
+    public void Update(float temperature, float pressure, float humidity);
+}
---
+++ ObserverPattern/2-ObserverImplementation/Observers/StatisticsDisplay.cs
@@ -0,0 +1,18 @@
+﻿namespace ObserverPattern._2_ObserverImplementation.Observers;
+
+public class StatisticsDisplay : IObserver, IDisplay
+{
+    private (float temperature, float humidity, float pressure) _data = (0, 0, 0);
+
+    public void Update(float temperature, float pressure, float humidity)
+    {
+        _data.pressure = pressure;
+        _data.humidity = humidity;
+        _data.temperature = temperature;
+    }
+
+    public string Display()
+    {
+        return $"Temp: {_data.temperature}, Pressure: {_data.pressure}, Humidity: {_data.humidity}";
+    }
+}
