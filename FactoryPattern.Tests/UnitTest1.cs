25acfc6e94220741dde859ac1fe61f63d982cfd6
mbrajk <mbrajk@users.noreply.github.com>
2022-06-03 00:52:39 -0500

    add factory pattern structure

---
+++ FactoryPattern.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryPattern.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
